eb9d433a2f4d35281f67a10c0ae01a505b3e3f28
Eduards Mukans <eduards.mukans@graip.ai>
2025-03-14 21:18:28 +0200

    Add stratified model report

--- train.py
+++ train.py
@@ -18,12 +18,13 @@ np.random.seed(42)
 
 MAX_LEN = 200
 batch_size = 64
+# epoch_count = 1
 epoch_count = 50
 learning_rate = 2e-5
 downsample_size = 1
 
 
-# checkpoint = "distilbert/distilbert-base-uncased"
+checkpoint = "distilbert/distilbert-base-uncased"
 # checkpoint = "cardiffnlp/tweet-topic-21-multi"
 # checkpoint = "cardiffnlp/twitter-roberta-base-sentiment-latest"
 # checkpoint = "cardiffnlp/twitter-roberta-large-topic-sentiment-latest"
@@ -32,7 +33,7 @@ downsample_size = 1
 # checkpoint = "microsoft/deberta-v2-xxlarge-mnli"
 # checkpoint = "Azie88/COVID_Vaccine_Tweet_sentiment_analysis_roberta"
 # checkpoint = "distilbert/distilbert-base-uncased-finetuned-sst-2-english"
-checkpoint = "papluca/xlm-roberta-base-language-detection"
+# checkpoint = "papluca/xlm-roberta-base-language-detection"
 
 train_dataset_dir = 'data/task1/train'
 dev_dataset_dir = 'data/task1/dev'
@@ -52,6 +53,7 @@ tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = AutoModelForSequenceClassification.from_pretrained(
     checkpoint, num_labels=len(id2label), id2label=id2label, label2id=label2id, ignore_mismatched_sizes=True
 )
+# model = AutoModelForSequenceClassification.from_pretrained('model/' + os.environ["WANDB_NAME"])
 
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 accuracy = evaluate.combine(["accuracy", "f1", "precision", "recall"])
@@ -67,6 +69,7 @@ wandb.init()
 
 ds_path = 'data/task1/plain_ds'
 dataset = DatasetDict.load_from_disk(ds_path)
+dev_df = dataset['dev'].to_pandas()
 test_df = dataset['test'].to_pandas()
 
 wandb.log({
@@ -92,11 +95,11 @@ training_args = TrainingArguments(
     per_device_eval_batch_size=batch_size,
     num_train_epochs=epoch_count,
     weight_decay=0.001,
-    evaluation_strategy="epoch",
+    eval_strategy="epoch",
     save_strategy="epoch",
     save_total_limit=2,
     load_best_model_at_end=True,
-    metric_for_best_model='eval_f1'
+    metric_for_best_model='eval_f1',
 )
 
 trainer = Trainer(
@@ -118,18 +121,24 @@ trainer.save_model("model/" + os.environ["WANDB_NAME"])
 predictions = trainer.predict(dataset['test'])
 test_df['prediction'] = np.argmax(predictions.predictions, axis=1)
 
+predictions = trainer.predict(dataset['dev'])
+dev_df['prediction'] = np.argmax(predictions.predictions, axis=1)
 
-def stratified_predictions(df, category):
+
+def stratified_predictions(df, category, split):
     result = dict()
-    for lang in df[category].unique():
-        df_to_test = df[df[category] == lang]
+    for cat_type in df[category].unique():
+        df_to_test = df[df[category] == cat_type]
 
-        r = accuracy.compute(predictions=df_to_test['predictions'], references=df_to_test['label'])
+        r = accuracy.compute(predictions=df_to_test['prediction'].tolist(), references=df_to_test['label'].tolist())
         for k, v in r.items():
-            result[category + '_' + k] = v
+            result[split + '/' + category + '/' + cat_type + '/' + k] = v
 
     return result
 
 
-wandb.log(stratified_predictions(test_df, 'language'))
-wandb.log(stratified_predictions(test_df, 'type'))
+wandb.log(stratified_predictions(dev_df, 'language', 'eval'))
+wandb.log(stratified_predictions(dev_df, 'type', 'eval'))
+
+wandb.log(stratified_predictions(test_df, 'language', 'test'))
+wandb.log(stratified_predictions(test_df, 'type', 'test'))
